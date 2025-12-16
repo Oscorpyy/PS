49edf89de3290a6c73be49a84feb0a320a6f3554
Oscar Pernod <opernod@z2r11p2.42lyon.fr>
2025-12-16 15:14:56 +0100

    disorder move

---
+++ sources/Bubble_sort.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bubble_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opernod <opernod@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 15:03:28 by opernod           #+#    #+#             */
+/*   Updated: 2025/12/16 15:13:20 by opernod          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ps.h"
+#include "../printf/includes/ft_printf.h"
+
+void	ft_buble_sort(int	*stack_a, int	*stack_b, int len_a, int len_b)
+{
+	while 
+}
