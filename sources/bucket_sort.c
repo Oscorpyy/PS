c2a542e53e2df95e1160aef148c29dddf93e87c1
Leo Goderneaux <lgoderne@z2r12p3.42lyon.fr>
2025-12-17 18:23:37 +0100

    buuuuuuuucket

---
+++ sources/bucket_sort.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bucket_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgoderne <lgoderne@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 16:27:04 by lgoderne          #+#    #+#             */
+/*   Updated: 2025/12/17 18:23:05 by lgoderne         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ps.h"
+
+int	ft_sqrt(int nb)
+{
+	int			little;
+	long long	mid;
+	int			big;
+
+	if (nb < 1)
+		return (0);
+	little = 0;
+	big = nb;
+	while (little < big)
+	{
+		mid = (little + big) / 2;
+		if (mid < nb)
+			little = mid + 1;
+		else if (mid * mid > nb)
+			big = mid - 1;
+		else
+			return ((int)mid);
+	}
+	return (0);
+}
+
+int	bucket_sort_simple(t_stack stack)
+{
+	int	num_buckets = ft_sqrt(stack.len_a);
+
+	
+}
