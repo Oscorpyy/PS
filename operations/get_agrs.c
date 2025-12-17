b039444671cec95f8634f5567d191efbea76bb11
Oscar Pernod <opernod@z2r12p2.42lyon.fr>
2025-12-17 14:44:59 +0100

    patch ra rb

--- operations/get_agrs.c
+++ operations/get_agrs.c
@@ -5,58 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: opernod <opernod@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/17 14:22:13 by opernod           #+#    #+#             */
-/*   Updated: 2025/12/17 14:27:43 by opernod          ###   ########lyon.fr   */
+/*   Created: 2025/12/17 14:40:30 by opernod           #+#    #+#             */
+/*   Updated: 2025/12/17 14:40:31 by opernod          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/ps.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int	get_size(char *args)
+int *read_file_to_array(const char *filename, int *size)
 {
-	int		fd;
-	int		size_dict;
-	int		reading;
-	char	buffer[3];
+    FILE *file;
+    int *array;
+    int value;
+    int count = 0;
+    int i = 0;
 
-	size_dict = 0;
-	reading = 1;
-	fd = open(args, O_RDONLY);
-	if (fd == -1)
-		write(2, " Dict Error\n", 12);
-	while (reading > 0)
-	{
-		reading = read(fd, buffer, 1);
-		size_dict ++;
-	}
-	close(fd);
-	return (size_dict);
-}
+    file = fopen(filename, "r");
+    if (!file)
+        return NULL;
 
-char	*dict_to_str(char *args)
-{
-	int		fd;
-	int		size_dict;
-	char	*str;
+    /* 1er passage : compter */
+    while (fscanf(file, "%d", &value) == 1)
+        count++;
 
-	fd = open(args, O_RDONLY);
-	if (fd == -1)
-		write(2, " Dict Error\n", 12);
-	size_dict = get_size(args);
-	str = malloc(sizeof(char) * size_dict);
-	if (!str)
-		return (NULL);
-	read(fd, str, size_dict);
-	close(fd);
-	return (str);
-}
+    if (count == 0)
+    {
+        fclose(file);
+        return NULL;
+    }
 
-t_stack	converte_arg(char	*args)
-{
-	char	*str;
-	int		i;
+    array = malloc(sizeof(int) * count);
+    if (!array)
+    {
+        fclose(file);
+        return NULL;
+    }
 
-	str = dict_to_str(args);
-	i = 0;
-	while ()
+    rewind(file);
+
+    /* 2e passage : remplir */
+    while (fscanf(file, "%d", &value) == 1)
+        array[i++] = value;
+
+    fclose(file);
+
+    *size = count;
+    return array;
 }
--- operations/test.c
+++ operations/test.c
@@ -6,15 +6,18 @@
 /*   By: opernod <opernod@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 12:26:53 by opernod           #+#    #+#             */
-/*   Updated: 2025/12/17 14:22:38 by opernod          ###   ########lyon.fr   */
+/*   Updated: 2025/12/17 14:44:55 by opernod          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ps.h"
 
+int *read_file_to_array(const char *filename, int *size);
 
 int	main(void)
 {
+	int size;
+	int *stack_a;
 	int		stack_b[500] = {};
 	int		i;
 	int		fd;
@@ -26,12 +29,7 @@ int	main(void)
 	stack->len_a = 500;
 	stack->len_b = 0;
 	i = 0;
-
-	fd = open(args.txt, O_RDONLY);
-
-
-
-	
+	stack_a = read_file_to_array("args.txt", &size);
 	disorder_per = disorder(stack);
 	total = selection_sort(stack);
 	while (i != stack->len_a)
