b21c6d52239cdcb8e681d9ec35c14cc4eb9675f9
Oscar Pernod <opernod@z2r12p2.42lyon.fr>
2025-12-17 15:55:54 +0100

    all

--- sources/adaptive.c
+++ sources/adaptive.c
@@ -1,30 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ra.c                                               :+:      :+:    :+:   */
+/*   adaptive.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: opernod <opernod@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/16 12:15:02 by opernod           #+#    #+#             */
-/*   Updated: 2025/12/17 12:06:35 by opernod          ###   ########lyon.fr   */
+/*   Created: 2025/12/17 15:46:46 by opernod           #+#    #+#             */
+/*   Updated: 2025/12/17 15:48:07 by opernod          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/ps.h"
+#include "../include/ps.h"
 
-void	rotate_a(t_stack *stack)
-{
-	int	i;
-	int	temp;
-
-	if (stack->stack_a == NULL || stack->len_a < 2)
-		return ;
-	i = 0;
-	temp = stack->stack_a[0];
-	while (i + 1 != stack->len_a)
-	{
-		stack->stack_a[i] = stack->stack_a[i + 1];
-		i++;
-	}
-	stack->stack_a[i] = temp;
-}
+void	adaptive_algo()
