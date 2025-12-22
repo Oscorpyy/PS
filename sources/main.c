e18e0c179e9716610528f05977160af7c70c5d62
pernod oscar <pernodoscar@gmail.com>
2025-12-22 19:02:10 +0100

    main with print disorder for check the sort

--- sources/main.c
+++ sources/main.c
@@ -79,6 +79,7 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	stack->stack_a = init_all(stack, argv, argc);
+	ft_printf("disorder before sort : %i\n", disorder(stack));
 	if (!stack->stack_a || only_one(stack) != 0 || mode(argv, stack) == 1)
 	{
 		free_all(stack, 1);
@@ -90,5 +91,6 @@ int	main(int argc, char **argv)
 		ft_printf("stack[%i] = %i \n", i, stack->stack_a[i]);
 		i++;
 	}
+	ft_printf("disorder after sort : %i\n", disorder(stack));
 	free_all(stack, 0);
 }
