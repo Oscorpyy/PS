10fa5e5596a8f4fa8f581ba2cc5f87219ada7e60
LeoGoder <l.goderneaux@ik.me>
2026-01-11 14:18:22 +0100

    normed new_argv fix leaks and context error

--- sources/new_argv.c
+++ sources/new_argv.c
@@ -44,59 +44,53 @@ static int	len_sub_part(char *sub_argv, int index)
 	while (sub_argv[index])
 	{
 		if (sub_argv[index] == ' ')
-			return (len);
+			return (len + 1);
 		index++;
 		len++;
 	}
-	return (len);
+	return (len + 1);
 }
 
-// static void	sub_part_creation(char	*sub_argv, int	*to_write, char	**final_argv)
-// {
-	
-// }
+static int	sub_part_creation(char *argv, int to_write, char **final_argv)
+{
+	int	y;
+	int	k;
+
+	y = 0;
+	while (argv[y])
+	{
+		final_argv[to_write] = malloc(sizeof(char) * len_sub_part(argv, y));
+		if (!final_argv[to_write])
+			return (-1);
+		k = 0;
+		while (argv[y] && argv[y] != ' ')
+			final_argv[to_write][k++] = argv[y++];
+		final_argv[to_write][k] = '\0';
+		to_write++;
+		if (argv[y] == ' ')
+			y++;
+	}
+	return (to_write);
+}
 
 char	**new_argv(char **argv, int *argc)
 {
 	int		len_subpart;
 	char	**final_argv;
 	int		i;
-	int		y;
-	int		k;
 	int		to_write;
 
 	len_subpart = total_subpart(argv, *argc);
-	final_argv = malloc(sizeof(char	*) * (len_subpart + 1));
+	final_argv = malloc(sizeof(char *) * (len_subpart + 1));
 	if (!final_argv)
 		return (NULL);
 	to_write = 0;
 	i = 0;
 	while (i < *argc)
 	{
-		y = 0;
-		k = 0;
-		final_argv[to_write] = malloc(sizeof(char) * (len_sub_part(argv[i], y) + 1));
-		if (!final_argv[to_write])
+		to_write = sub_part_creation(argv[i], to_write, final_argv);
+		if (to_write == -1)
 			return (NULL);
-		while (argv[i][y])
-		{
-			if (argv[i][y] == ' ')
-			{
-				final_argv[to_write][k] = '\0';
-				to_write += 1;
-				y++;
-				k = 0;
-				final_argv[to_write] = malloc(sizeof(char) * (len_sub_part(argv[i], y) + 1));
-				if (!final_argv)
-					return (NULL);
-				continue;
-			}
-			final_argv[to_write][k] = argv[i][y];
-			k++;
-			y++;
-		}
-		final_argv[to_write][k] = '\0';
-		to_write += 1;
 		i++;
 	}
 	final_argv[to_write] = NULL;
